8ab9f870094cc10df700f4ff691b04d09f69855e
QICHANG BAO <bao.jason1990@gmail.com>
2020-11-10 15:07:33 -0500

    update footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-class Footer extends React.Component{
-    render(){
-        return (     
-            <footer className="page-footer font-small blue">
-                <div className="footer-copyright text-center py-3">© 2020 Copyright:
-                    <a href="https://www.hntb.com/">HNTB</a>
-                </div>  
-            </footer>
-        )
-    }
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="page-footer font-small blue">
+        <div className="footer-copyright text-center py-3">
+          © 2020 Copyright:
+          <a href="#">Qichang Bao</a>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
